8de77e43148fbf122551a187eb7113e4b20d54a6
haydencarroll1 <carrollhayden8@gmail.com>
2024-04-10 13:37:44 +0100

    bug fix

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -10,10 +10,10 @@ public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             String dictionaryPath = "resources/words.txt";
-            Indexer indexer = new Indexer(dictionaryPath);
+            Indexer indexer = new Indexer();
             Search search = new Search(indexer);
             SpellChecker spellChecker = new SpellChecker(dictionaryPath); // Create instance of SpellChecker
-            SearchUI searchUI = new SearchUI(search, indexer, spellChecker);
+            SearchUI searchUI = new SearchUI(search, spellChecker);
             searchUI.setVisible(true);
         });
     }
